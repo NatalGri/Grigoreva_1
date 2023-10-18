78fedf21cd138dbe71f4652b6549f758e55f9695
Natasha Grigoreva <karabatova.n.v@mail.ru>
2023-10-19 01:12:11 +0500

    Добавлены решения задач

--- task17/Program.cs
+++ task17/Program.cs
@@ -1,2 +1,63 @@
 ﻿/* 17. Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт
 номер четверти плоскости, в которой находится эта точка.*/
+
+System.Console.Write("Введите координату Х: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Введите координату Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+    System.Console.WriteLine("Точка находится в первой четверти");
+}
+else if (x < 0 && y > 0)
+{
+    System.Console.WriteLine("Точка находится во второй четверти");
+}
+else if (x < 0 && y < 0)
+{
+    System.Console.WriteLine("Точка находится в третьей четверти");
+}
+else if (x > 0 && y < 0)
+{
+    System.Console.WriteLine("Точка лежит в четвертной четверти");
+}
+else
+{
+    System.Console.WriteLine("Ошибка! \nОдна из координат равна нулю");
+}
+
+
+// решение задачи с помощью массива
+
+/* int[] coord = new int[2];
+
+string[] msg = { "Точка находится в первой четверти", "Точка находится во второй четверти", "Точка находится в третьей четверти", "Точка лежит в четвертной четверти", "Ошибка! \nОдна из координат равна нулю" };
+
+System.Console.Write("Введите координату Х: ");
+coord[0] = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Введите координату Y: ");
+coord[1] = Convert.ToInt32(Console.ReadLine());
+
+if (coord[0] > 0 && coord[1] > 0)
+{
+    System.Console.WriteLine(msg[0]);
+}
+else if (coord[0] < 0 && coord[1] > 0)
+{
+    System.Console.WriteLine(msg[1]);
+}
+else if (coord[0] < 0 && coord[1] < 0)
+{
+    System.Console.WriteLine(msg[2]);
+}
+else if (coord[0] > 0 && coord[1] < 0)
+{
+    System.Console.WriteLine(msg[3]);
+}
+else
+{
+    System.Console.WriteLine(msg[4]);
+} */
--- task18/Program.cs
+++ task18/Program.cs
@@ -1,2 +1,66 @@
 ﻿/* Задача 18: Напишите программу, которая по заданному номеру четверти, показывает
 диапазон возможных координат точек в этой четверти (x и y).*/
+
+System.Console.Write("Введите номер четверти от 1 до 4: ");
+int chet = Convert.ToInt32(Console.ReadLine());
+
+string [] arrayMsg = {"x > 0, y > 0", "x < 0, y > 0", "x < 0, y < 0", "x > 0, y < 0", "Ошибка! \nТакой четверти нет."};
+
+if (chet == 1)
+{
+    System.Console.WriteLine(arrayMsg[0]);
+}
+else if (chet == 2)
+{
+    System.Console.WriteLine(arrayMsg[1]);
+}
+else if (chet == 3)
+{
+    System.Console.WriteLine(arrayMsg[2]);
+}
+else if (chet == 4)
+{
+    System.Console.WriteLine(arrayMsg[3]);
+}
+else
+{
+    System.Console.WriteLine(arrayMsg[4]);
+}
+
+
+/* Console.WriteLine("Введите номер четверти от 1 до 4"); // второй способ
+int chet = Convert.ToInt32(Console.ReadLine());
+string[] arrayMsg = { "x > 0, y > 0", "x < 0, y > 0", "x < 0, y < 0", "x > 0, y < 0", "Ошибка!" };
+
+switch (chet) // switch - case - используется, когда мы ожидаем конкретное значение, а не диапазон
+{
+    case 1:
+        System.Console.WriteLine(arrayMsg[0]);
+        break;
+    case 2:
+        System.Console.WriteLine(arrayMsg[1]);
+        break;
+    case 3:
+        System.Console.WriteLine(arrayMsg[2]);
+        break;
+    case 4:
+        System.Console.WriteLine(arrayMsg[3]);
+        break;
+    default:
+        System.Console.WriteLine(arrayMsg[4]);
+        break;
+}  */
+
+
+/* Console.WriteLine("Введите номер четверти от 1 до 4"); // третий способ
+int chet = Convert.ToInt32(Console.ReadLine()); 
+string[] arraystr = { "x > 0, y > 0", "x < 0, y > 0", "x < 0, y < 0", "x > 0, y < 0", "Ошибка!" };
+
+if (chet > 0 && chet < 5)
+{
+System.Console.WriteLine(arraystr[chet - 1]);
+}
+else
+{
+System.Console.WriteLine(arraystr[4]);
+} */
