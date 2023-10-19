f5193302b432671a1b4a98ddc2341788d7bdd1f2
Natasha Grigoreva <karabatova.n.v@mail.ru>
2023-10-19 16:45:32 +0500

    Добавлены решения задач

--- task20/Program.cs
+++ task20/Program.cs
@@ -2,3 +2,30 @@
 находит расстояние между ними в 2D пространстве.
 A (3,6); B (2,1) -> 5,09
 A (7,-5); B (1,-1) -> 7,21 */
+
+int[] A = new int[2];
+System.Console.Write("Введите координату Х точки А: ");
+A[0] = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите координату Y точки А: ");
+A[1] = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine();
+
+int[] B = new int[2];
+System.Console.Write("Введите координату Х точки B: ");
+B[0] = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите координату Y точки B: ");
+B[1] = Convert.ToInt32(Console.ReadLine());
+
+int resX = (int) Math.Pow(A[0] - B[0], 2);
+// int resX = Convert.ToInt32(Math.Pow(A[0] - B[0], 2));
+
+int resY = (int) Math.Pow(A[1] - B[1], 2);
+
+double distance = Math.Sqrt(resX + resY);
+
+System.Console.WriteLine(Math.Round(distance, 2));
+
+// Math.Pow(value1, value2) -- возведение value1 в степень value2
+// Math.Round(value1, value2) -- округляет value1 до value2 знаков после запятой
+//Math.Sqrt() -> взятие квадратного корня
--- task22/Program.cs
+++ task22/Program.cs
@@ -3,3 +3,16 @@
 5 -> 1, 4, 9, 16, 25.
 2 -> 1, 4 */
 
+System.Console.Write("Введите число: ");
+int N = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+for(int i = 1; i<=N; i++)
+System.Console.Write(Math.Pow(i, 2) + " ");
+
+
+// int i = 1;
+// while(i <= N)
+// {
+// System.Console.Write(Math.Pow(i, 2) + " ");
+// i++; // i = i + 1 ; i+=1
+// }
