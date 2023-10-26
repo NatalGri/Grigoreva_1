defd0b6f7e4676dc5cc32176176e512fe062fef4
Natasha Grigoreva <karabatova.n.v@mail.ru>
2023-10-26 09:06:43 +0500

    Добавлено решение задачи 34

--- task34/Program.cs
+++ task34/Program.cs
@@ -4,4 +4,20 @@
 массиве.
 [345, 897, 568, 234] -> 2 */
 
+int[] GenerateArray(int sizeArray, int leftRange; int rightRange)
+{
+    int[] newArray = new int[sizeArray];
+    Random rand = new Random();
 
+    for(int i = 0; i < newArray.Length; i++)
+    {
+        newArray[i] = rand.Next();
+    }
+}
+
+
+
+
+
+int [] array = GenerateArray(5, 100, 999)
+PrintArray(array);
