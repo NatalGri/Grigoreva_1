d7d9e1f509f9f30baec49ea5fbce91dc6897efda
Natasha Grigoreva <karabatova.n.v@mail.ru>
2023-10-27 12:03:02 +0500

    добавлены решения задач 41, 43

--- task41/Program.cs
+++ task41/Program.cs
@@ -3,4 +3,21 @@
 0, 7, 8, -2, -2 -> 2
 -1, -7, 567, 89, 223-> 3 */
 
+int PositiveNum(int[] newArray)
+{
+    int count = 0;
 
+    for(int i = 0; i < newArray.Length; i++)
+    {
+        if(newArray[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+System.Console.Write("Введите целые числа: ");
+int[] array = Array.ConvertAll(Console.ReadLine()!.Split(", "), int.Parse);
+System.Console.WriteLine("Количество цифр больше 0: " + PositiveNum(array));
--- task43/Program.cs
+++ task43/Program.cs
@@ -3,3 +3,24 @@
 значения b1, k1, b2 и k2 задаются пользователем.
 
 b1 = 2, k1 = 5, b2 = 4, k2 = 9   ->   (-0,5; -0,5) */
+
+
+double[] Point (int[] newArray)
+{
+    double[] arrayPoint = new double[2];
+    
+    arrayPoint[0] = Math.Round(((double)(newArray[3] - newArray[1]) / (newArray[0] - newArray[2])), 1);
+    arrayPoint[1] = Math.Round(((double)newArray[0] * arrayPoint[0] + newArray[1]), 1);
+    
+    return arrayPoint;   
+}
+
+
+System.Console.Write("Введите значения k1, b1 и k2, b2 по порядку: ");
+int[] array = Array.ConvertAll(Console.ReadLine()!.Split(" "), int.Parse);
+
+if(array[0] != array[2])
+    System.Console.WriteLine("Точка пересечения двух прямых: " + "(" + string.Join("; ", Point(array)) + ")");
+else
+    System.Console.WriteLine("Значения коэффициентов k не могут быть равны");
+
