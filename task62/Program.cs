7b5754069f1efda4ce3e614053e775a715e64564
Natasha Grigoreva <karabatova.n.v@mail.ru>
2023-11-03 11:24:10 +0500

    добавлены задачи

---
+++ task62/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Задача 62: Заполните спирально массив 4 на 4.
+
+1   2   3   4
+12  13  14  5
+11  16  15  6
+10  9   8   7 */
+
+int[,] GenerateMatrix(int rows, int cols)
+{
+    int[,] newMatrix = new int[rows, cols];
+    int count = 1;
+
+    for (int i = 0; i < newMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < newMatrix.GetLength(1); j++)
+        {
+            newMatrix[i, j] = count;
+            count++;
+            
+        }
+    }
+    return newMatrix;
+}
+
+
+void PrintMatrix(int[,] matrixForPrint)
+{
+    for (int i = 0; i < matrixForPrint.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixForPrint.GetLength(1); j++)
+        {
+            System.Console.Write(matrixForPrint[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] matrix = GenerateMatrix(4, 4);
+PrintMatrix(matrix);
