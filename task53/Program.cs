015bd03f5305e5d713fc975526f4197bf0b73c6a
Natasha Grigoreva <karabatova.n.v@mail.ru>
2023-11-15 08:48:52 +0500

    добавлены задачи

--- task53/Program.cs
+++ task53/Program.cs
@@ -2,4 +2,50 @@
 которая поменяет местами первую и последнюю строку
 массива. */
 
+int[,] GenerateMatrix(int row, int col)
+{
+    Random rand = new Random();
+    int[,] matrix = new int[row, col];
 
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            matrix[i, j] = rand.Next(-9, 10);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void ReverseMatrix(int[,] matrix)
+{
+    int temp;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        temp = matrix[0, i];
+        matrix[0, i] = matrix[matrix.GetLength(0) - 1, i];
+        matrix[matrix.GetLength(0) - 1, i] = temp;
+    }
+}
+
+// --------------------------------------------
+
+int[,] myMatrix = GenerateMatrix(4, 4);
+PrintMatrix(myMatrix);
+ReverseMatrix(myMatrix);
+System.Console.WriteLine();
+System.Console.WriteLine();
+PrintMatrix(myMatrix);
--- task55/Program.cs
+++ task55/Program.cs
@@ -2,3 +2,55 @@
 которая заменяет строки на столбцы. В случае, если это
 невозможно, программа должна вывести сообщение для
 пользователя. */
+
+int[,] GenerateMatrix(int row, int col)
+{
+    Random rand = new Random();
+    int[,] matrix = new int[row, col];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            matrix[i, j] = rand.Next(-9, 10);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
+int[,] RotateMatrix(int[,] matrix)
+{
+    int[,] tempMatrix = new int[matrix.GetLength(1), matrix.GetLength(0)];
+
+    for (int i = 0; i < tempMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < tempMatrix.GetLength(1); j++)
+        {
+            tempMatrix[i, j] = matrix[j, i];
+        }
+    }
+    return tempMatrix;
+}
+
+// --------------------------------------------
+
+int[,] myMatrix = GenerateMatrix(5, 5);
+
+PrintMatrix(myMatrix);
+
+System.Console.WriteLine();
+PrintMatrix(RotateMatrix(myMatrix));
